1a7a9a6e9aa0b59758028b83550cd0da5569b0e4
brudex <brudexgh@gmail.com>
2024-02-12 16:07:06 +0000

    db env vars

--- src/database/config/database.js
+++ src/database/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
 	development: {
-		username: "postgres",
-		password: "pass@1234",
-		database: "database_development",
-		host: "127.0.0.1",
+		username: process.env.DBUSER ||  "postgres",
+		password: process.env.DBPASS ||   "pass@1234",
+		database:  process.env.DBNAME ||   "database_development",
+		host:  process.env.DBHOST || "127.0.0.1",
 		dialect: "postgres",
 		operatorsAliases: 0,
 		define: {
@@ -25,10 +25,21 @@ module.exports = {
 		dialect: "",
 	},
 	production: {
-		username: "",
-		password: "",
-		database: "",
-		host: "",
-		dialect: "",
+		username: process.env.DBUSER ||  "postgres",
+		password: process.env.DBPASS ||   "pass@1234",
+		database:  process.env.DBNAME ||   "database_development",
+		host:  process.env.DBHOST || "127.0.0.1",
+		dialect: "postgres",
+		operatorsAliases: 0,
+		define: {
+			timestamp: true,
+			underscored: true,
+		},
+		pool: {
+			max: 5,
+			min: 0,
+			acquire: 30000,
+			idle: 1000
+		}
 	},
 };
